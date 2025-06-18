606c83e5f2f434c724024e40ee1e2f449da30233
mood7dev <mood7dev@gmail.com>
2025-06-18 14:06:33 +0900

    vue 연동방법 공부

---
+++ src/components/Tire.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  color: String,
+});
+</script>
+
+<template>
+  <!-- :style 이것은 vue문법
+            중괄호 2개 겹쳐져 있는 것은 콧수염(Mustache 템플릿)
+    -->
+  <div :style="{ color: props.color || 'black' }">
+    {{ props.color || "BLACK" }} TIRE
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Component.vue
@@ -0,0 +1,18 @@
+<script setup>
+import FrontWheel from "@/components/FrontWheel.vue";
+import RearWheel from "@/components/RearWheel.vue";
+</script>
+
+<template>
+  <div class="car">
+    <FrontWheel />
+    <RearWheel />
+  </div>
+</template>
+
+<style scoped>
+.car {
+  display: flex;
+  gap: 10px;
+}
+</style>
